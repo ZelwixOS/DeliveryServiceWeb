e1799093c4a78b061b4c5ff872a437441354da87
Zelwix <vafin_01@mail.ru>
2021-12-16 17:04:45 +0300

    test 0.3.0
    Add integration test for GetUsersByRole function

---
+++ DeliveryService/Tests.BLL/dbOperationsTests/GetUsersByRole.cs
@@ -0,0 +1,45 @@
+﻿using BLL.Interfaces;
+using BLL.Models;
+using DAL;
+using FakeTestClasses;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.BLL.dbOperationsTests
+{
+    public class GetUsersByRoleTests
+    {
+        private BLLFactory factory = new BLLFactory();
+
+
+        [Fact]
+        public void GetUsersByRole_MoqObjectWithThreeUsers_TwoUsersOneInEachRole()
+        {
+            User customer = UserUtility.GetCustomer();
+            User courier = UserUtility.GetCustomer();
+            User admin = UserUtility.GetAdmin();
+
+            var mock = new Mock<IAccountService>();
+
+            mock.Setup(i => i.GetByRole("courier")).Returns(Task.FromResult((IList<User>)(new List<User> { courier })));
+            mock.Setup(i => i.GetByRole("customer")).Returns(Task.FromResult((IList<User>)(new List<User> { customer })));
+            mock.Setup(i => i.GetByRole("admin")).Returns(Task.FromResult((IList<User>)(new List<User> { admin })));
+            var serviceMock = mock.Object;
+
+            UsersByRole expected = new UsersByRole();
+            expected.Customers.Add(new UserModel(customer));
+            expected.Couriers.Add(new UserModel(courier));
+
+            var dbOperations = factory.CreateDbOperations();
+
+
+            var result = dbOperations.GetUsersByRole(serviceMock);
+
+
+            Assert.Equal(expected.Couriers[0].ID, result.Couriers[0].ID);
+            Assert.Equal(expected.Customers[0].ID, result.Customers[0].ID);
+        }
+    }
+}
